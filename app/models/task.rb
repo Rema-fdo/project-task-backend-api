0a0877395ed2e1a1ec04ca7ec70c89634994a1e3
Rema Fernando <remafdo181299@gmail.com>
2025-12-25 02:08:04 +0530

    Add associations for task_members and users in Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,6 @@ class Task < ApplicationRecord
   belongs_to :priority
   belongs_to :project
   has_many :attachments, as: :attachable, dependent: :destroy
+  has_many :task_members, dependent: :destroy
+  has_many :users, through: :task_members
 end
